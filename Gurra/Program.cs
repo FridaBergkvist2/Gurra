a0e47f0725439a0bd370e39a1a7f0ceadebab372
FridaBergkvist2 <fridaber10@student.astar.se>
2025-08-12 11:39:50 +0200

    uppdaterat filerna med information

--- Gurra/Program.cs
+++ Gurra/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+	static void Main ()
+	{
+		List<Produkt> produkter = new List<Produkt>();
+
+		produkter.Add(new Produkt ("Teskedsgumman", 345));
+
+		bool kör = true;
+
+		while (kör)
+		{
+			try
+			{
+				Console.Write("Ange tavla, eller skriv 'slut':");
+				string namn = Console.ReadLine();
+
+				if(namn.ToLower()=="slut")
+				{
+					kör = false;
+					continue;
+				}
+
+				Console.Write("Kostnad:");
+				double pris = Convert.ToDouble (Console.ReadLine());
+		
+				produkter.Add(new Produkt (namn,pris));
+			}
+
+			catch (FormatException)
+			{
+				Console.WriteLine("Du måste ange ett pris");
+			}
+
+		}
+
+		Console.WriteLine("\nLista på tavlor");
+		foreach (Produkt p in produkter)
+		{
+			p.VisaInfo();
+		}
+	}
+}
